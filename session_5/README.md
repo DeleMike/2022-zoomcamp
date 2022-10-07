778dab1152ca59795e04c8c4993d68f1530d8854
DeleMike <akindelemichael65@gmail.com>
2022-10-07 19:49:06 +0100

    work on session 5 homework

---
+++ session_5/README.md
@@ -0,0 +1,7 @@
+# Session 5
+
+## Deploying ML Model and Docker
+
+Knowing how to deploy models so that it is useful for other people to use is really important.
+
+We can deploy models on GCP, AWS, Azure, Heroku, PythonAnyWhere.
---
+++ session_5/predict.py
@@ -0,0 +1,38 @@
+import pickle
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+model_file = 'model1.bin'
+dv_file = "dv.bin"
+print('loading model and dv...')
+
+with open(model_file, 'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open(dv_file, 'rb') as f_in2:
+    dv = pickle.load(f_in2)
+
+print('Loaded model: ', (dv, model))
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    client = request.get_json()
+     
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0,1]
+    default = y_pred >=0.5
+
+    result = {
+        'default_probability':float(y_pred),
+        'default':bool(default)
+    }
+    return jsonify(y_pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
